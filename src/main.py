03d762a47a0fd8c28eee313c8eb03d0c142472d5
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-17 14:29:54 -0400

    created setStartDate()

--- src/main.py
+++ src/main.py
@@ -7,12 +7,20 @@ month = int(today.strftime("%m"))
 day = int(today.strftime("%d"))
 year = int(today.strftime("%Y"))
 
+def setStartDate():
+    print("Enter date correctly, without quotes E.g. '2005, 11, 05'")
+    startDate = input(">")
+
+    with open('date.txt', 'w') as saveDate:
+        saveDate.write(startDate)
+
 def main():
     system("clear")
-    startDate = date(1964, 8, 14)
-    today = date(year, month, day)
-    delta = today - startDate
-    print(f"Days passed since {startDate}: {delta.days}")
+    
+    # startDate = date(1964, 8, 14)
+    # today = date(year, month, day)
+    # delta = today - startDate
+    # print(f"Days passed since {startDate}: {delta.days}")
 
 if __name__ == "__main__":
     main()
