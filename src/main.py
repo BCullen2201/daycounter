94f8172a676451cc0d1f966f5313bad0bc3eec7d
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-17 14:20:03 -0400

    Get current date, find difference

--- src/main.py
+++ src/main.py
@@ -2,8 +2,17 @@ from os import system
 from sys import exit
 from datetime import date
 
+today = date.today()
+month = int(today.strftime("%m"))
+day = int(today.strftime("%d"))
+year = int(today.strftime("%Y"))
+
 def main():
-    return
+    system("clear")
+    startDate = date(1964, 8, 14)
+    today = date(year, month, day)
+    delta = today - startDate
+    print(f"Days passed since {startDate}: {delta.days}")
 
 if __name__ == "__main__":
     main()
