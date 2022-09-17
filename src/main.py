40d13c186dec02b22f2d14c3bfda652538b72afc
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-17 14:49:38 -0400

    created showDifference()

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from datetime import date
 welcomeMessage = """Day Counter
 
 1 - Set start date
-2 - Exit
+2 - Show days passed
+3 - Exit
 """
 
 today = date.today()
@@ -14,11 +15,26 @@ day = int(today.strftime("%d"))
 year = int(today.strftime("%Y"))
 
 def setStartDate():
-    print("Enter date correctly, without quotes E.g. '2005, 11, 05'")
+    system("clear")
+    print("Enter date correctly, without quotes, in YYYY MM DD E.g. '2005, 11, 05'")
     startDate = input(">")
 
     with open('date.txt', 'w') as saveDate:
         saveDate.write(startDate)
+
+def showDifference():
+    system("clear")
+    try:
+        getStartDate = open('date.txt', 'r')
+    except Exception:
+        print("No start date, please create one!")
+        return
+    startDate = getStartDate.read()
+    
+    x = date(int(startDate))
+    today = date(year, month, day)
+    delta = today - x
+    print(f"Days passed since {startDate}: {delta.days}")
 
 def main():
     system("clear")
@@ -27,6 +43,8 @@ def main():
     if userChoice == "1":
         setStartDate()
     elif userChoice == "2":
+        showDifference()
+    elif userChoice == "3":
         userIsSure = input("Are you sure you want to quit? Y or N: ")
         if userIsSure == "Y" or userIsSure == "y":
             exit(0)
