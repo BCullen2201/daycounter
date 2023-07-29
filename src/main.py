f42b198a2d4c11164a3adab2db3f480c77ab9acb
Brandon Cullen <brandoncullen2201@gmail.com>
2023-07-29 12:41:15 -0400

    Moved while loop inside main

--- src/main.py
+++ src/main.py
@@ -48,25 +48,25 @@ def showDifference():
     print(f"Days passed since {startDate}: {delta.days}\n")
 
 def main():
-    system("clear")
-    print(f"{mainMenu}")
-    showDifference()
-    userChoice = input(">")
-    if userChoice == "1":
-        setStartDate()
-    elif userChoice == "2":
+    while True:
         system("clear")
-        userIsSure = input("Are you sure you want to quit? Y or N: ")
-        if userIsSure == "Y" or userIsSure == "y":
-            exit(0)
-        elif userIsSure == "N" or userIsSure == "n":
-            return
+        print(f"{mainMenu}")
+        showDifference()
+        userChoice = input(">")
+        if userChoice == "1":
+            setStartDate()
+        elif userChoice == "2":
+            system("clear")
+            userIsSure = input("Are you sure you want to quit? Y or N: ")
+            if userIsSure == "Y" or userIsSure == "y":
+                exit(0)
+            elif userIsSure == "N" or userIsSure == "n":
+                return
+            else:
+                return
         else:
+            input("Not a valid input! Press ENTER to continue")
             return
-    else:
-        input("Not a valid input! Press ENTER to continue")
-        return
 
 if __name__ == "__main__":
-    while True:
-        main()
+    main()
